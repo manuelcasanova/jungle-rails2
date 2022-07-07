cdc8fa367f2f4753789299294f1d47db396e9d78
Manuel Casanova <manucasanova@hotmail.com>
2022-07-07 12:32:26 -0700

    Refactor. Move functions to sale.rb

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,2 +1,16 @@
 class Sale < ApplicationRecord
+
+  def finished?
+    ends_on < Date.current
+  end
+
+  def upcoming?
+    starts_on > Date.current
+  end
+
+  #We don't need active function for the admin all sales status but we might need it at a later point.
+  def active?
+    !upcoming? && !finished?
+  end
+
 end
