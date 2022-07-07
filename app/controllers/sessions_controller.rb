11d54fdbfe2446a195672123f90fe827f0897726
Manuel Casanova <manucasanova@hotmail.com>
2022-07-06 17:14:15 -0700

    Update sessions controller with the logic to log users in and out. Update the application controller with methods to look up the user. Show user name and last name if logget in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,7 @@ class SessionsController < ApplicationController
   def new
   end
 
+  
   def create
     user = User.find_by_email(params[:email])
     # If the user exists AND the password entered is correct.
