e102729b410419103e6a8a5930f8098f1978fd70
Manuel Casanova <manucasanova@hotmail.com>
2022-07-18 12:33:23 -0700

    Add tests. Product is valid when created with valid attributes. Product is not valid without name

---
+++ spec/product_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+
+  let(:category) {
+    Category.create(name: "testCategory")
+  }
+  subject {
+    category.products.create(
+      name: "test_product",
+      description: "a_test_product",
+      image: "test_image",
+      price_cents: 10000,
+      quantity: 10,
+    )
+  }
+
+  describe "Validations" do
+    it "is valid with valid attributes" do
+      expect(subject).to be_valid
+      expect(subject.errors.full_messages).to be_empty
+    end
+
+    it "is not valid without a name" do
+      subject.name = nil
+      expect(subject).to_not be_valid
+      expect(subject.errors.full_messages.first).to eq "Name cannot be empty"
+    end
+
+
+  end
+end
