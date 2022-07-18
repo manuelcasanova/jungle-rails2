464da6d38ba84cf09847fccda3bc168823fbc7b8
Manuel Casanova <manucasanova@hotmail.com>
2022-07-18 13:12:34 -0700

    Add validation of presente of first name, last name, password and password confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
 end
