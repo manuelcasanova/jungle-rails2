925971bdcaf38610aa6ddf5cd1fe9d2aa2b2ccad
Manuel Casanova <manucasanova@hotmail.com>
2022-07-18 13:16:54 -0700

    Add test. Not valid without first name, last name, password or password confirmation

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -23,5 +23,40 @@ RSpec.describe User, type: :model do
       expect(same_user.errors.full_messages).to include ("Email has already been taken")
     end
 
+    it "is not valid without an email" do
+      subject.email = nil
+      expect(subject).to_not be_valid
+      expect(subject.errors.full_messages).to include ("Email can't be blank")
+    end
+
+    it "is not valid without first name" do
+      user = User.create( 
+        last_name: "Casanova", 
+        email: "manucasanova@hotmail.com", 
+        password: "secret", 
+        password_confirmation: "secret"
+      )
+      expect(user).to_not be_valid
+      expect(user.errors.full_messages).to include ("First name can't be blank")
+    end
+
+    it "is not valid without last name" do
+      subject.last_name = nil
+      expect(subject).to_not be_valid
+      expect(subject.errors.full_messages).to include ("Last name can't be blank")
+    end
+
+    it "is not valid without pasword" do
+      subject.password = nil
+      expect(subject).to_not be_valid
+      expect(subject.errors.full_messages).to include ("Password can't be blank")
+    end
+
+    it "is not valid without pasword confirmation" do
+      subject.password_confirmation = nil
+      expect(subject).to_not be_valid
+      expect(subject.errors.full_messages).to include ("Password confirmation can't be blank")
+    end
+
   end
 end
