ccb0b15de0025b0ce42b1c95e90d41047902e746
Manuel Casanova <manucasanova@hotmail.com>
2022-07-18 12:53:15 -0700

    Fix error messages. Add .to include missing in one of the tests

--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -24,18 +24,19 @@ RSpec.describe Product, type: :model do
     it "is not valid without a name" do
       subject.name = nil
       expect(subject).to_not be_valid
-      expect(subject.errors.full_messages.first).include "Name cannot be empty"
+      expect(subject.errors.full_messages.first).to include "Name can't be blank"
     end
 
     it "is not valid without a price" do
       subject.price_cents = nil
       expect(subject).to_not be_valid
-      expect(subject.errors.full_messages).to include "Price cannot be empty"
+      expect(subject.errors.full_messages).to include "Price can't be blank"
+    end
 
     it "is not valid without a quantity" do
       subject.quantity = nil
       expect(subject).to_not be_valid
-      expect(subject.errors.full_messages).to include "Quantity cannot be empty"
+      expect(subject.errors.full_messages).to include "Quantity can't be blank"
     end
 
     it "is not valid without a category" do
@@ -47,7 +48,7 @@ RSpec.describe Product, type: :model do
         quantity: 10,
       )
       expect(product).to_not be_valid
-      expect(product.errors.full_messages).to include "Category cannot be empty"
+      expect(product.errors.full_messages).to include "Category can't be blank"
     end
 
   end
