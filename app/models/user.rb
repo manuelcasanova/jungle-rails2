9922a6497e0783440fc28a4f86177ba958708b8a
Manuel Casanova <manucasanova@hotmail.com>
2022-07-18 14:02:11 -0700

    Add email.strip.downcase for validation with spaces and wrong case

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
 
 
   def self.authenticate_with_credentials(email, password)
-    user = User.find_by_email(email)
+    user = User.find_by_email(email.strip.downcase)
     if user && user.authenticate(password)
       user
     else
