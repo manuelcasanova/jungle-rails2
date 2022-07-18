429becdd9bf27521dcd4b1893c5d7026fe975888
Manuel Casanova <manucasanova@hotmail.com>
2022-07-18 12:35:42 -0700

    Add test. Product is not valid without a price

--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -24,8 +24,13 @@ RSpec.describe Product, type: :model do
     it "is not valid without a name" do
       subject.name = nil
       expect(subject).to_not be_valid
-      expect(subject.errors.full_messages.first).to eq "Name cannot be empty"
+      expect(subject.errors.full_messages.first).include "Name cannot be empty"
     end
+
+    it "is not valid without a price" do
+      subject.price_cents = nil
+      expect(subject).to_not be_valid
+      expect(subject.errors.full_messages).to include "Price cannot be empty"
 
 
   end
