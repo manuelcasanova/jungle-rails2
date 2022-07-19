c2181da061ca83032827c6d5bf0bf1d9c3d74a90
Manuel Casanova <manucasanova@hotmail.com>
2022-07-19 08:49:36 -0700

    Modify cypress initializer to create a category and some products when Cypress starts. Add a test to validate the amount of products

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -24,5 +24,9 @@ describe('jungle rails app', () => {
     cy.get(".products article").should("be.visible");
   });
 
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 12);
+  });
+
 
 })
